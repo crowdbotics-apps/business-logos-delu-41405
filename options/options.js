ba147b05029b4e76bdc11d4407fd81e2719ad1f9
Crowdbotics <team@crowdbotics.com>
2023-06-10 17:53:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "business-logos-delu-41405",
+  url: "https://business-logos-delu-41405.botics.co",
+  api: "https://business-logos-delu-41405.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
